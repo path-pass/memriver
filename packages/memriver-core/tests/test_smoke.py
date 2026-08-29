cec95ba3dced2cfd93b4ecc7a97d6dbb30b6a145
zzzdw <zdw85203456@gmail.com>
2026-08-29 09:41:30 +0900

    chore: scaffold uv workspace with memriver-core and memriver packages

---
+++ packages/memriver-core/tests/test_smoke.py
@@ -0,0 +1,5 @@
+import memriver_core
+
+
+def test_version():
+    assert memriver_core.__version__ == "0.1.0"
---
+++ packages/memriver/tests/test_smoke.py
@@ -0,0 +1,5 @@
+import memriver
+
+
+def test_version():
+    assert memriver.__version__ == "0.1.0"
